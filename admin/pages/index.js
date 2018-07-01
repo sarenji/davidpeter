f6f2e8051c98a5c5ab0770a8108f4c48990923f5
David Peter <david.a.peter@gmail.com>
2018-06-30 23:46:02 -0400

    Use react-no-ssr instead of a hack.

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,11 +1,12 @@
 import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate, PostIcon } from 'admin/resources/posts';
 import jsonServerProvider from 'ra-data-json-server';
+import NoSSR from 'react-no-ssr';
 
 export default () => (
-  <div>{process.browser &&
+  <NoSSR>
     <Admin dataProvider={jsonServerProvider('http://localhost:3002')}>
       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
     </Admin>
-  }</div>
+  </NoSSR>
 );
