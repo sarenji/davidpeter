aecd74ef5dd18955ed825d0966dca708e1436973
David Peter <david.a.peter@gmail.com>
2018-06-30 23:48:02 -0400

    Minor refactoring to add a TODO.

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -3,9 +3,12 @@ import { PostList, PostEdit, PostCreate, PostIcon } from 'admin/resources/posts'
 import jsonServerProvider from 'ra-data-json-server';
 import NoSSR from 'react-no-ssr';
 
+// TODO: Find a way to not hardcode the port/address.
+const provider = jsonServerProvider('http://localhost:3002');
+
 export default () => (
   <NoSSR>
-    <Admin dataProvider={jsonServerProvider('http://localhost:3002')}>
+    <Admin dataProvider={provider}>
       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
     </Admin>
   </NoSSR>
